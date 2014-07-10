0e2d4b6805213fae7396442766910b4595baaf86
Lucas Cotner <lc07@uw.edu>
2014-07-10 16:11:51 -0700

    Passing Casper Acceptance Tests

--- nodeRewrite/Gruntfile.js
+++ nodeRewrite/Gruntfile.js
@@ -3,17 +3,52 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   
   grunt.initConfig({
+    express: {
+      options: {
+        // Override defaults here
+      },
+      dev: {
+        options: {
+          script: 'server.js'
+        }
+      },
+      prod: {
+        options: {
+          script: 'server.js',
+          node_env: 'production'
+        }
+      },
+      test: {
+        options: {
+          script: 'server.js'
+        }
+      }
+    },
     jshint: {
       all: ['Gruntfile.js', 'server.js']
     },
     simplemocha: {
 	  options: { timeout: 3000 },
 	    all: { src: ['test/*.js'] }
-	}
+	  },
+    casper: {
+      acceptance : {
+        options : {
+          test : true,
+        },
+        files : {
+          'test/acceptance/casper-results.xml' : ['test/acceptance/*_test.js']
+        }
+      }
+    }
+
   });
 
   grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-express-server');
+  grunt.loadNpmTasks('grunt-casper');
 
+  grunt.registerTask('server', [ 'jshint', 'express:dev' ]);
   grunt.registerTask('default','jshint');
-  grunt.registerTask('test','simplemocha');
+  grunt.registerTask('test', ['simplemocha','express:dev','casper']);
 };
