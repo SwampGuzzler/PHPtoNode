4b85ec3c6ebc3801ce3db43dd4eaefd60add9b0c
Lucas Cotner <lc07@uw.edu>
2014-07-18 14:34:56 -0700

    Finally understand why we weren't serving our app.js file correctly, and the ramifications of changing the directory we use in our server.js file

--- nodeRewrite/server.js
+++ nodeRewrite/server.js
@@ -5,6 +5,12 @@ var http    = require('http');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/dist'));
+// So above, if we use dist, we can reference the browserified 'app.js' file easily, but we 
+// no longer get what's in the public folder (which is all that logic I built in), we just get 
+// some lame static html and our other files alongside it. -Basically, we'd have to re-build 
+// the logic there using the app.js bundle rather than explicit stand-alone js files alongside 
+// our index.html file
 
 var server = http.createServer(app);
 server.listen(3000, function() {
